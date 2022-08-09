664d5cfbd511478fc3f03bf173ce84553815ba46
jnw031 <108843025+jnw031@users.noreply.github.com>
2022-08-09 11:50:16 +0800

    0

--- js/main.js
+++ js/main.js
@@ -11,6 +11,35 @@ var time = time || 0;
 $("html,body").animate({scrollTop: $("#"+id).offset().top}, time);
 }
 
+//核心价值观 点击
+var a_idx = 0; 
+jQuery(document).ready(function($) { 
+    $("body").click(function(e) { 
+        var a = new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤"); 
+        var $i = $("<span></span>").text(a[a_idx]); 
+        a_idx = (a_idx + 1) % a.length; 
+        var x = e.pageX, 
+        y = e.pageY; 
+        $i.css({ 
+            "z-index": 999999, 
+            "top": y - 20, 
+            "left": x, 
+            "position": "absolute", 
+            "font-weight": "bold", 
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")" 
+        }); 
+        $("body").append($i); 
+        $i.animate({ 
+            "top": y - 180, 
+            "opacity": 0 
+        }, 
+        1500, 
+        function() { 
+            $i.remove(); 
+        }); 
+    }); 
+}); 
+
 //change menu-style 
 var showmenu = false;
 $(document).scroll(function() {
