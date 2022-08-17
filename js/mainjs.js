c3feed43a14c4680f5057ba0a49e7db7d9bd7721
jnw031 <108843025+jnw031@users.noreply.github.com>
2022-08-17 16:16:54 +0800

    0

--- js/mainjs.js
+++ js/mainjs.js
@@ -63,7 +63,7 @@ var showmenu = false;
 			arr0[int] = randomNum(1,18);
 			while(isRepeat(arr0)){arr0[int] = randomNum(1,18);}//防止图片重复
 			var github = 'https://jnw031.github.io/img/banner_bg';
-			var imgURL = github + arr0[int] +".webp";
+			var imgURL = github + arr0[int] +".jpg";
 			//[div]document.getElementById("BackgroundArea").style.backgroundImage="url("+imgURL+")";
 			document.getElementById("img"+int).src= imgURL;
 			preloadImg([imgURL]);
